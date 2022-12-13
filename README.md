786e3bae3d4ae2cca0f91df241578c8ef3ba31eb
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-13 06:38:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,153 @@
+# HAR Sanitizer
+
+## Description
+
+HAR files are JSON-formatted "recordings" of web traffic activity for a user's browser session, which are often used to troubleshoot web front-ends, REST APIs, authentication issues, etc.  However, HAR files will capture everything in a web session, including passwords, sensitive form information, authentication cookies and headers, and any content embedded in HTTP requests.  This makes HAR files extremely sensitive, and highly prone to privacy breaches if handled incorrectly.
+
+This tool aims to help mitigate these concerns by offering a simple, flexible interface to redact HAR file contents of any potentially sensitive information.  It collects the names and values of all passwords, cookies, headers, URLQuery/POSTData/HTML-Form parameters, and embedded content mimetypes, and redacts values either already known to be sensitive, or those specified by the user.  It currently exists as a both a client-side web tool and Flask REST API.
+
+Live version may be found at https://har-sanitizer.appspot.com/
+
+(This is NOT an official Google product)
+
+## Installation
+
+```
+$ git clone https://github.com/google/har-sanitizer.git
+$ cd har-sanitizer
+$ virtualenv -p $(which python2.7) venv --no-site-packages
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+#### Local Flask site (CLI @ root "./har-sanitizer/" directory)
+
+1. (If virtual environment not already activated)
+```
+$ source venv/bin/activate
+```
+
+2. If desired, change static files location in config.json. Examples:
+```
+{
+  "static_files": "./harsanitizer/static"
+}
+
+-or-
+
+{
+  "static_files": "https://storage.googleapis.com/har-sanitizer/static"
+}
+
+```
+
+3. Change port, debug, and other options in ./harsanitizer/harsan_api.py under:
+```
+app.run(...)
+```
+
+4. Launch Flask server:
+```
+$ PYTHONPATH=. python ./harsanitizer/harsan_api.py
+```
+
+5. Load the Har-Sanitizer web tool by visiting "http://localhost:8080" in Chrome or Firefox (substituting '8080' with the port #, if modified).
+
+
+## Usage
+
+#### Web Tool
+
+1. Load HAR JSON file using 'Load HAR' button.
+
+2. Select names of cookies/headers/parameters/content mimetypes to scrub.
+
+3. Preview changes before committing, modifying scrub options as necessary.
+
+4. Export scrubbed HAR file once ready.
+
+#### API Endpoint
+
+* /get_wordlist - Returns default HarSanitizer wordlist.
+
+* /default_mimetype_scrublist - Returns default HarSanitizer mimeTypes scrub list.
+
+* /cookies - Returns all cookie names found in POSTed Har (json). Example (Python w/ 'requests' package):
+  ```
+  import json, requests
+  with open("har_file.har", "r") as har_file:
+      har = json.load(har_file)
+  url = 'http://localhost:8080/cookies'
+  headers = {"Content-Type": "application/json"}
+  r = requests.post(url, data=json.dumps(har), headers=headers)
+  ```
+
+* /headers - Returns all header names found in POSTed Har (json). See /cookies for example.
+
+* /params - Returns all URL Query and POSTData Parameter names found in POSTed Har (json).  See /cookies for example.
+
+* /mimetypes - Returns all content mimeTypes found in POSTed Har (json). See /cookies for example.
+
+* /scrub_har - Full scrub/redaction of sensitive HAR fields.
+
+  Args:
+
+    * har: the har json to be scrubbed
+
+    * wordlist=None, (list of strs) appends to default wordlist
+
+    * content_list=None, (list of strs) appends to default content_list
+
+    * all_cookies=False,  (Boolean) Redacts all cookies
+
+    * all_headers=False, (Boolean) Redacts all headers
+
+    * all_params=False, (Boolean) Redacts all URLQuery/POSTData parameters
+
+    * all_content_mimetypes=False (Boolean) Redacts all content mimeTypes
+
+    Example:
+
+    ```
+    import json, requests
+    with open("har_file.har", "r") as har_file:
+      har = json.load(har_file)
+    url = 'http://localhost:8080/scrub_har'
+    headers = {"Content-Type": "application/json"}
+    data = {"har": har, "wordlist": ['mycookie', 'mycookie2'], "all_params": True}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    ```
+
+## TODO
+
+1. Needs tests bad.  This should be current priority.  Use pytest and Jasmine.
+
+2. Other issues are being tracked in Github [issue tracker](https://github.com/google/har-sanitizer/issues).
+
+## Contact
+
+Garrett Anderson: 
+* garrett@nulldestinations.com
+
+Greg Cochard: 
+* gcochard@google.com
+
+Geoffrey Coulter: 
+* gcoulter@google.com
+
+## License
+Copyright 2017, Google Inc.
+
+Authors: Garrett Anderson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   <http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
---
+++ harsanitizer/harsan_api.py
@@ -0,0 +1,222 @@
+"""Scans and sanitzes HAR files containing sensitive information."""
+
+# Copyright 2017, Google Inc.
+# Authors: Garrett Anderson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import datetime
+import json
+import urllib2
+from flask import Flask, url_for, request, Response, render_template_string
+import decorators
+from harsanitizer import Har, HarSanitizer
+
+
+# Config local/remote file locations
+CURRENT_DIR = os.path.abspath("./")
+
+# Load/sanity check config.json
+try:
+  with open("./config.json", "r") as config:
+    STATIC_FOLDER = json.load(config)["static_folder"]
+except IOError:
+  raise IOError(
+    "'config.json' not found in '{}'. Please ensure that script is "
+    "being run from root './har-sanitizer/' directory.".format(CURRENT_DIR))
+except KeyError:
+  raise KeyError("'STATIC_FOLDER' key not found in config.json")
+
+WORDLIST_PATH = "{}/wordlist.json".format(STATIC_FOLDER)
+MIMETYPES_PATH = "{}/mimetypesScrubList.json".format(STATIC_FOLDER)
+# Local STATIC_FOLDER and template config
+if STATIC_FOLDER[:4] != "http":
+  INDEX_PATH = "{}/templates/localhost/index.html".format(STATIC_FOLDER)
+# Remote STATIC_FOLDER and template config
+else:
+  INDEX_PATH = "{}/templates/remotehost/index.html".format(STATIC_FOLDER)
+
+
+# Serialize utility
+def json_serial(obj):
+  """JSON serializer for datetime.datetime not serializable by default json code."""
+  if isinstance(obj, datetime.datetime):
+    serial = obj.isoformat()
+    return serial
+  raise TypeError("Object not of type datetime.datetime")
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  if STATIC_FOLDER[:4] == "http":
+    index_html_str = urllib2.urlopen(INDEX_PATH).read()
+  else:
+    with open(INDEX_PATH, "r") as index_file:
+      index_html_str = index_file.read()
+  return render_template_string(index_html_str, static_files=STATIC_FOLDER)
+
+@app.route("/get_wordlist", methods=["GET"])
+def get_wordlist():
+  """Returns default HarSanitizer wordlist."""
+  hs = HarSanitizer()
+
+  try:
+    if WORDLIST_PATH[:4] == "http":
+      wordlist_json = json.loads(urllib2.urlopen(WORDLIST_PATH).read())
+      wordlist = hs.load_wordlist(wordlist=wordlist_json)
+    else:
+      wordlist = hs.load_wordlist(wordlist_path=WORDLIST_PATH)
+  except Exception:
+    message = {"message": "Error: {} not found.".format(WORDLIST_PATH)}
+    data = json.dumps(message, default=json_serial)
+    return Response(data, 500, mimetype="application/json")
+
+  data = json.dumps(wordlist, default=json_serial)
+  return Response(data, 200, mimetype="application/json")
+
+
+@app.route("/default_mimetype_scrublist", methods=["GET"])
+def get_mimetype_scrublist():
+  """Returns default HarSanitizer mimeTypes scrub list."""
+  hs = HarSanitizer()
+
+  try:
+    if MIMETYPES_PATH[:4] == "http":
+      mimetype_scrub_list = json.loads(urllib2.urlopen(MIMETYPES_PATH).read())
+    else:
+      with open(MIMETYPES_PATH, "r") as mimetypes_file:
+        mimetype_scrub_list = json.load(mimetypes_file)
+  except Exception:
+    message = {"message": "Error: {} not found.".format(MIMETYPES_PATH)}
+    data = json.dumps(message, default=json_serial)
+    return Response(data, 500, mimetype="application/json")
+
+  data = json.dumps(mimetype_scrub_list, default=json_serial)
+  return Response(data, 200, mimetype="application/json")
+
+
+@app.route("/cookies", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def req_cookie_names():
+  """Returns all cookie names found in POSTed Har (json)."""
+  data = request.json
+  hs = HarSanitizer()
+
+  har = Har(har=data)
+  cookies = hs.get_hartype_names(har, "cookies").keys()
+
+  data = json.dumps(cookies, default=json_serial)
+
+  return Response(data, 200, mimetype="application/json")
+
+
+@app.route("/headers", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def req_header_names():
+  """Returns all header names found in POSTed Har (json)."""
+  data = request.json
+  hs = HarSanitizer()
+
+  har = Har(har=data)
+  headers = hs.get_hartype_names(har, "headers").keys()
+
+  data = json.dumps(headers, default=json_serial)
+
+  return Response(data, 200, mimetype="application/json")
+
+
+@app.route("/params", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def req_urlparams():
+  """Returns all URL Query and POSTData Parameter names found in POSTed Har (json)."""
+  data = request.json
+  hs = HarSanitizer()
+  cond_table = {}
+
+  har = Har(har=data)
+  url_pattern = hs.gen_hartype_names_pattern(har, "queryString")
+  postdata_pattern = hs.gen_hartype_names_pattern(har, "params")
+  cond_table.update(url_pattern)
+  cond_table.update(postdata_pattern)
+  iter_har_dict = hs.iter_eval_exec(my_iter=har.har_dict, cond_table=cond_table)
+  har = hs.har
+  urlparams = har.category["queryString"].keys()
+
+  if isinstance(har.category["params"].keys(), list):
+    postdata_params = har.category["params"].keys()
+    params = urlparams + postdata_params
+  else:
+    params = urlparams
+
+  data = json.dumps(params, default=json_serial)
+
+  return Response(data, 200, mimetype="application/json")
+
+
+@app.route("/mimetypes", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def req_mimetypes():
+  """Returns all content mimeTypes found in POSTed Har (json)."""
+  data = request.json
+  hs = HarSanitizer()
+
+  har = Har(har=data)
+  mimetypes = hs.get_mimetypes(har).keys()
+
+  data = json.dumps(mimetypes, default=json_serial)
+
+  return Response(data, 200, mimetype="application/json")
+
+
+@app.route("/scrub_har", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def scrub():
+  """Scrubs data["har"] with optional wordlists,
+  content types, and scrub_all type bools.
+  """
+  hs = HarSanitizer()
+  hs_kwargs = {}
+
+  data = request.json
+  har = Har(har=data["har"])
+
+  if "wordlist" in data.keys():
+    hs_kwargs["wordlist"] = data["wordlist"]
+  if "content_list" in data.keys():
+    hs_kwargs["content_list"] = data["content_list"]
+  if "all_cookies" in data.keys():
+    hs_kwargs["all_cookies"] = data["all_cookies"]
+  if "all_headers" in data.keys():
+    hs_kwargs["all_headers"] = data["all_headers"]
+  if "all_params" in data.keys():
+    hs_kwargs["all_params"] = data["all_params"]
+  if "all_content_mimetypes" in data.keys():
+    hs_kwargs["all_content_mimetypes"] = data["all_content_mimetypes"]
+
+  sanitized_har = hs.scrub(har, **hs_kwargs)
+
+  data = json.dumps(sanitized_har.har_dict, indent=2, separators=(",", ": "))
+  return Response(data, 200, mimetype="text/plain")
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=8080, debug=False)
